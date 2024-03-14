4d416ed1c21502d24a1fa7e5bd97cecbfb516bab
10922234 <161452740+10922234@users.noreply.github.com>
2024-03-14 16:27:15 +0800

    總之就是ok了吧

--- 自己用/20230314_3.py
+++ 自己用/20230314_3.py
@@ -7,11 +7,5 @@ df_csv = pd.read_csv(r'C:\Users\waes3\Downloads\原始檔案\TDCS_M05A_20240228_
 df_csv = pd.read_csv(r'C:\Users\waes3\Downloads\原始檔案\TDCS_M04A_20240228_000000_1.csv')
 
 
-
-# 使用 merge 函數來合併'GantryFrom'和'GantryTo'的相同文字的資料
-merged_df = pd.merge(df_csv, df_csv, left_on=['GantryFrom', 'GantryTo'], right_on=['GantryFrom', 'GantryTo'], how='outer')
-
-
-
-# 將合併後的 DataFrame 儲存為新的 CSV 檔案
-merged_df.to_csv(r'C:\Users\waes3\Downloads\merged.csv', index=False)
+#擷取欄位'TravelTime'的資料，顯示前5筆
+print(df_csv['TravelTime'].head())
