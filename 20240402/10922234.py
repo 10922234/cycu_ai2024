285d26c2bc1aaabf18cffc03740148b79a725e5a
10922234 <161452740+10922234@users.noreply.github.com>
2024-04-02 18:52:37 +0800

    0402

--- 20240402/10922234.py
+++ 20240402/10922234.py
@@ -4,13 +4,12 @@
 #read 20240402/tract_20140313.json as geopanas
 import geopandas as gpd
 import pandas as pd
-# read shape file 'C:\Users\User\Downloads\COUNTY_MOI_1090820.shp'
-shape_file = 'C:\\Users\\User\\Downloads\\COUNTY_MOI_1090820.shp'
-taiwan = gpd.read_file(shape_file)
 
+#read shape file
+taiwan=gpd.read_file('C:\Users\User\Downloads\mapdata202301070205\COUNTY_MOI_1090820')
 import os
 #print cwd
-print (taiwan)
+print (taiwan.shape)
 
 #plot taiwan using matplotlib
 import matplotlib.pyplot as plt
